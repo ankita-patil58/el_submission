1bf7cf8dece727beaef6ea965cd06865313dc030
ankita-patil58 <ankitapatil581999@gmail.com>
2025-03-14 14:41:09 +0530

    Create requirements.R

---
+++ requirements.R
@@ -0,0 +1,17 @@
+# Install the necessary packages with specific versions
+install.packages("remotes")
+
+# Install CRAN packages with specified versions
+remotes::install_version("shiny", version = "1.7.4")
+remotes::install_version("shinyalert", version = "3.0.0")
+remotes::install_version("plotly", version = "4.10.1")
+remotes::install_version("reactable", version = "0.4.4")
+remotes::install_version("edgeR", version = "3.40.2")
+remotes::install_version("limma", version = "3.54.2")
+remotes::install_version("shinyhelper", version = "0.3.2")
+remotes::install_version("shinyjs", version = "2.1.0")
+remotes::install_version("ggplot2", version = "3.4.1")
+remotes::install_version("DT", version = "0.27")
+remotes::install_version("data.table", version = "1.14.8")
+remotes::install_version("shinydashboard", version = "0.7.2")
+remotes::install_github("ElucidataInc/mapGCT")
